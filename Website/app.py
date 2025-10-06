bd56b2f09d6fb16c37caf4d975a1efc858781f58
ThomasamohT <thomasrieger32@gmail.com>
2025-10-06 16:02:13 +0700

    Update app.py

--- Website/app.py
+++ Website/app.py
@@ -2,7 +2,6 @@ import streamlit as st
 st.set_page_config(page_title="Historical Text Analyzer", layout="wide")
 import pandas as pd
 import os
-from Website.utils_old import load_ner_model
 from user_page import run_user_page
 
 run_user_page()
